78c8d3fe5b6f4f0e50ee7727742a52cb2d0be0e2
Ousmane <bachirmahbarke@gmail.com>
2023-02-16 22:01:32 +0100

    loveMangas

--- average.js
+++ average.js
@@ -1,12 +1,14 @@
 function average() {
   var notes = [20, 12, 8, 9];
     var sum = 0;
+    let average1=0;
   
     for (var i = 0; i < notes.length; i++) {
       sum += notes[i];
     }
-  
-    return sum / notes.length;
+     average1 = sum / notes.length;
+    
+    return average1;
     
     
   }
