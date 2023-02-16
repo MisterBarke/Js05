1070c9aeae9e0fe33b3373b32f6691ed74fae585
MisterBarke <121509658+MisterBarke@users.noreply.github.com>
2023-02-16 23:55:39 +0100

    loveMangas

--- average.js
+++ average.js
@@ -1,14 +1,14 @@
 function average() {
   var notes = [20, 12, 8, 9];
     var sum = 0;
-    let average1=0;
+   
   
     for (var i = 0; i < notes.length; i++) {
       sum += notes[i];
     }
-     average1 = sum / notes.length;
+     return sum / notes.length;
     
-    return average1;
+    
     
     
   }
